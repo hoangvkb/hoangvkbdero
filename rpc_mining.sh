a8069eb63c17e831de34b90f08fe5da4dd88d6d3
hoangvkb <128590561+hoangvkb@users.noreply.github.com>
2023-04-07 14:58:32 +0700

    Add files via upload

---
+++ rpc_mining.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w dero1qyxel4gq4uq276vs8gqhyckzxdgdxk0my4q4nr50zf6nw3ppncc8sqgcqnas6 -r 192.168.68.130:10100 r1 comunity-pools.mysrv.cloud:10300 r2 dero.friendspool.club:10300 -p rpc;
+    sleep 5;
+done
