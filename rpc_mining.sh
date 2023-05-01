17f5cac5a41af80e9f4f0ffc350d68ddd633dd31
hoangvkb <128590561+hoangvkb@users.noreply.github.com>
2023-05-02 00:07:12 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyxel4gq4uq276vs8gqhyckzxdgdxk0my4q4nr50zf6nw3ppncc8sqgcqnas6 -r 192.168.68.171:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.friendspool.club:10300 -p rpc;
+    ./astrominer -w dero1qyxel4gq4uq276vs8gqhyckzxdgdxk0my4q4nr50zf6nw3ppncc8sqgcqnas6 -r nodent2.cpumining.cloud:10100 -p rpc;
     sleep 5;
 done
